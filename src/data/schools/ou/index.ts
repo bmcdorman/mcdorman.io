bf5e32f8f92c7171524ab649a35af28a09795214
Braden McDorman <bmcdorman@gmail.com>
2022-10-19 11:40:57 -0700

    Add logos

--- src/data/schools/ou/index.ts
+++ src/data/schools/ou/index.ts
@@ -1,7 +1,11 @@
 import School from '../../../model/School';
 
+// @ts-ignore
+import logoUri from 'url:./logo.png';
+
 export default {
   id: 'ou',
   name: 'University of Oklahoma',
   url: 'https://www.ou.edu/',
+  logoUri
 } as School;
--- src/data/schools/usc/index.ts
+++ src/data/schools/usc/index.ts
@@ -1,8 +1,11 @@
 import School from '../../../model/School';
 
+// @ts-ignore
+import logoUri from 'url:./logo.png';
+
 export default {
   id: 'usc',
   name: 'University of Southern California',
   url: 'https://www.usc.edu/',
-  logoUri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/USC_seal.svg/1200px-USC_seal.svg.png',
+  logoUri,
 } as School;
